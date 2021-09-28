66e43b02fdf76cfffd7d2dc34fc02350dd5770ab
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-09-27 21:38:47 -0500

    spacing adjustments

--- models/Product.js
+++ models/Product.js
@@ -3,6 +3,7 @@ const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
+
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
